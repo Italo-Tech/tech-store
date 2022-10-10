870d0762c767507b32f90b5b85fa541192ccc464
italo <italo@appbee.online>
2022-10-10 14:54:07 -0300

    First commit

---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="">
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    setup: () => ({
+
+    }),
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex justify-center mt-24">
+        Nada
+    </div>
+</template>
+
+<script>
+export default {
+    setup: () => ({
+
+    }),
+};
+</script>
+
+<style scoped>
+/* CSS Here! */
+</style>
